97b43ed418e5182c4ea72b5130839cb1b2d75ba0
Daniel Richards <daniel.richards@automattic.com>
2018-11-14 17:40:40 +0800

    Refactor autosave state into core-data
    - Migrate state from core/editor to core
    - Adjust selectors and action creators to take a post id.
    - Store autosave state by the post id.
    - Deprecate old action creators and selectors and forward to new implementation
    - Update tests
    - Update changelog
    
    Add a resolver for fetching autosave data
    
    Preload any autosaves for the current post
    
    Improve isEditedPostAutosaveable selector deprecation
    
    Pass entire parent post object into autosave selector functions to allow the getAutosave resolver to form a REST API path that incorporates both the post id and type
    
    Use a selector to get the correct post type base url for the autosave
    
    Rename resetAutosave to receiveAutosave for conformity
    
    Render the post preview button as disabled until autosave has been fetched.
    
    Comments/test description tidy up
    
    Prefer `action` over `action creator` in copy
    
    Remove unecessary if statement
    
    Remove `autosave` prop overriding the prop of the same name in withDispatch
    
    Fix deprecated resetAutosave function
    
    Fix jsdoc formatting
    
    Remove new getAutosaveAttribute in favour of deprecating and removing old version
    
    Change autosave selector interface from func( post ) to func( postType, postId )
    
    Store raw autosave from REST API in core, move transformation of autosave back to the core/editor package
    
    Remove and deprecate hasAutosave in favour of getAutosave
    
    Update docs
    
    Rename reducer autosave -> autosaves
    
    Fix erroneous changelog entry caused by bad merge conflict resolution
    
    Update deprecations
    
    Remove unnecessary new line
    
    Use createRegistrySelector to select autosave data from correct registry for deprecated functions
    
    Handle usage of registry selector in tests for isEditedPostAutosaveable

--- lib/client-assets.php
+++ lib/client-assets.php
@@ -686,6 +686,7 @@ function gutenberg_editor_scripts_and_styles( $hook ) {
 		sprintf( '/wp/v2/users/me?post_type=%s&context=edit', $post_type ),
 		array( '/wp/v2/media', 'OPTIONS' ),
 		array( '/wp/v2/blocks', 'OPTIONS' ),
+		sprintf( '/wp/v2/%s/%s/autosaves?context=edit', $rest_base, $post->ID ),
 	);
 
 	/**
